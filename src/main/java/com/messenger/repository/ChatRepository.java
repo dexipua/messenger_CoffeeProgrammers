07b0bd3d1e52993112bbbc0be18b1b925730fbdb
VadymHoncharuk <vadymhoncharuk06@gmail.com>
2024-07-21 11:57:20 +0300

    add @Param

--- src/main/java/com/messenger/repository/ChatRepository.java
+++ src/main/java/com/messenger/repository/ChatRepository.java
@@ -3,10 +3,11 @@ package com.messenger.repository;
 import com.messenger.models.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     @Query("SELECT a.chats FROM Account a WHERE a.id =:id")
-    List<Chat> findAllByAccountId(Long id);
+    List<Chat> findAllByAccountId(@Param("id") Long id);
 }
