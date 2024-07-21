cff93a9a91eb0683bde18b66d656f2c4bd946f70
Vlad <bulakovskijvladislav@gmail.com>
2024-07-21 13:13:22 +0300

    Created tests for AuthDTO

---
+++ src/test/java/com/messenger/dto/auth/LoginRequestDTOTest.java
@@ -0,0 +1,22 @@
+package com.messenger.dto.auth;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoginRequestDTOTest {
+
+    @Test
+    void testData() {
+        LoginRequestDTO loginRequestDTO1 = Instancio.create(LoginRequestDTO.class);
+
+        LoginRequestDTO loginRequestDTO2 = new LoginRequestDTO();
+        loginRequestDTO2.setUsername(loginRequestDTO1.getUsername());
+        loginRequestDTO2.setPassword(loginRequestDTO1.getPassword());
+
+        assertEquals(loginRequestDTO1, loginRequestDTO2);
+        assertEquals(loginRequestDTO1.hashCode(), loginRequestDTO2.hashCode());
+        assertEquals(loginRequestDTO1.toString(), loginRequestDTO2.toString());
+    }
+}
