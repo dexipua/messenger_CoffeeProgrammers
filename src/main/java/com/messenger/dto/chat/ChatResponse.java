9513decf5d05eb044f7be330163cadb9764d6288
VadymHoncharuk <vadymhoncharuk06@gmail.com>
2024-07-21 11:56:44 +0300

    add getters

--- src/main/java/com/messenger/dto/chat/ChatResponse.java
+++ src/main/java/com/messenger/dto/chat/ChatResponse.java
@@ -4,11 +4,13 @@ import com.messenger.models.Account;
 import com.messenger.models.Message;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@Getter
 public class ChatResponse {
     private Long id;
     private List<Message> messages;
