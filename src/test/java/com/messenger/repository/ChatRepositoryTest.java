5dd643ac2712b47be0310bdef5ff0ffc408825a4
VadymHoncharuk <vadymhoncharuk06@gmail.com>
2024-07-21 13:29:21 +0300

    add ChatRepositoryTest

---
+++ src/test/java/com/messenger/repository/ChatRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.messenger.repository;
+
+import com.messenger.models.Account;
+import com.messenger.models.Chat;
+import com.messenger.models.Role;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class ChatRepositoryTest {
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @AfterEach
+    void tearDown() {
+        chatRepository.deleteAll();
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    void findAllByAccountId() {
+        // given
+        Account account = new Account();
+        account.setEmail("email@gmil.com");
+        account.setPassword("passWord1");
+        account.setDescription("Some");
+        account.setFirstName("First");
+        account.setLastName("Last");
+        account.setRole(Role.USER);
+        accountRepository.save(account);
+
+        Chat chat1 = new Chat();
+        chat1.getAccounts().add(account);
+        Chat chat2 = new Chat();
+        chat2.getAccounts().add(account);
+        chatRepository.save(chat1);
+        chatRepository.save(chat2);
+
+        account.getChats().add(chat1);
+        account.getChats().add(chat2);
+        accountRepository.save(account);
+
+        // when
+        List<Chat> foundChats = chatRepository.findAllByAccountId(account.getId());
+
+        // then
+        assertThat(foundChats).hasSize(2);
+        //assertThat(foundChats).containsExactlyInAnyOrder(chat1, chat2);
+    }
+}
