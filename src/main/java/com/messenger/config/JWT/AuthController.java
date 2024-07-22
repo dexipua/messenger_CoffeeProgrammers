7ce80f7ee797201ea9d644dd1ed2d3b3bae1dc55
Artem Mosiechenko <ma.ua@outlook.com>
2024-07-22 07:54:37 +0300

    Add check token valid endpoint

--- src/main/java/com/messenger/config/JWT/AuthController.java
+++ src/main/java/com/messenger/config/JWT/AuthController.java
@@ -11,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -51,5 +48,10 @@ public class AuthController {
         String jwtToken = jwtUtils.generateTokenFromUsername(user.getUsername());
         return new AuthResponseDTO(user.getId(), user.getLastName(), user.getFirstName(),
                 user.getUsername(), jwtToken, user.getRole().name());
+    }
+
+    @GetMapping("/check")
+    public boolean checkTokenValid(String token) {
+        return jwtUtils.validateJwtToken(token);
     }
 }
