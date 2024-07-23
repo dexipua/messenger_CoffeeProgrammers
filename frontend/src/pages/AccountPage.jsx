59c2f415924373c50dd3da9c8a17ef507e1ea33b
Artem Mosiechenko <ma.ua@outlook.com>
2024-07-24 00:25:55 +0300

    Add AccountPage.jsx

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,47 +1,166 @@
 import React, {useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
-import {Box, Paper} from "@mui/material";
-import {useNavigate, useParams} from "react-router-dom";
+import {Box, Dialog, DialogTitle, Paper, TextField} from "@mui/material";
+import {useParams} from "react-router-dom";
 import AccountService from "../services/AccountService";
+import Cookies from "js-cookie";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from '@mui/icons-material/Edit';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import CloseIcon from '@mui/icons-material/Close';
 
 const AccountPage = () => {
-    const {id} = useParams();
+    const { id } = useParams();
 
-    const [description, setDescription] = useState();
-    const [email, setEmail] = useState();
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [isMy, setIsMy] = useState(false);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [description, setDescription] = useState("");
 
-    const get = async () => {
+    const [firstNameToUpdate, setFirstNameToUpdate] = useState("");
+    const [lastNameToUpdate, setLastNameToUpdate] = useState("");
+    const [descriptionToUpdate, setDescriptionToUpdate] = useState("");
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const get = async () => {
+            try {
+                const response = await AccountService.getById(id);
+                console.log(id);
+                console.log('Do successful:', response);
+
+                setFirstName(response.firstName);
+                setLastName(response.lastName);
+                setEmail(response.email);
+                setDescription(response.description);
+                setFirstNameToUpdate(response.firstName);
+                setLastNameToUpdate(response.lastName);
+                setDescriptionToUpdate(response.description);
+
+                setIsMy(response.id.toString() === Cookies.get('id'));
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        get();
+    }, [id]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleCloseUpdate = async () => {
         try {
-            const response = await AccountService.getById(id)
-            console.log(id)
+            // Оновлення акаунта
+            const response = await AccountService.update(firstNameToUpdate,
+                lastNameToUpdate, descriptionToUpdate);
+            console.log(id);
             console.log('Do successful:', response);
-            setFirstName(response.firstName)
-            setLastName(response.lastName)
-            setEmail(response.email)
-            setDescription(response.description)
+
+            // Оновлення стану тільки після успішного оновлення
+            setFirstName(response.firstName);
+            setLastName(response.lastName);
+            setEmail(response.email);
+            setDescription(response.description);
+            setIsMy(response.id.toString() === Cookies.get('id'));
         } catch (error) {
             setError(error.message);
         } finally {
             setLoading(false);
+            setOpen(false); // Закриття діалогу
         }
     };
 
-    useEffect(() => {
-        get();
-    }, []);
-
     if (loading) {
-        <p>Loading...</p>
+        return <p>Loading...</p>;
     }
 
     if (error) {
-        <p>Error</p>
+        return <p>Error: {error}</p>;
     }
+
+    const updateDia = () => {
+        if (isMy) {
+            return (
+                <Box>
+                    <IconButton onClick={handleClickOpen}><EditIcon /></IconButton>
+                    <Dialog
+                        aria-labelledby="customized-dialog-title"
+                        open={open}
+                        sx={{ width: 400 }}
+                    >
+                        <Box>
+                            <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                                Update
+                            </DialogTitle>
+                            <TextField
+                                sx={{ margin: 2, width: 300 }}
+                                label="First name"
+                                type="text"
+                                id="firstNameToUpdate"
+                                value={firstNameToUpdate}
+                                onChange={(e) => setFirstNameToUpdate(e.target.value)}
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                sx={{ margin: 2, width: 300 }}
+                                label="Last name"
+                                type="text"
+                                id="lastNameToUpdate"
+                                value={lastNameToUpdate}
+                                onChange={(e) => setLastNameToUpdate(e.target.value)}
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                sx={{ margin: 2, width: 300 }}
+                                label="Description"
+                                type="text"
+                                id="descriptionToUpdate"
+                                value={descriptionToUpdate}
+                                onChange={(e) => setDescriptionToUpdate(e.target.value)}
+                                required
+                                multiline
+                                maxRows={3}
+                                margin="normal"
+                            />
+                            <IconButton
+                                aria-label="close"
+                                onClick={handleClose}
+                                sx={{
+                                    position: 'absolute',
+                                    right: 8,
+                                    top: 8,
+                                    color: (theme) => theme.palette.grey[500],
+                                }}
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogActions>
+                                <Button autoFocus onClick={handleCloseUpdate}>
+                                    Save changes
+                                </Button>
+                            </DialogActions>
+                        </Box>
+                    </Dialog>
+                </Box>
+            );
+        }
+        return null;
+    };
 
     return (
         <Box
@@ -52,7 +171,7 @@ const AccountPage = () => {
             alignItems="center"
             justifyContent="center"
         >
-            <Paper sx={{backgroundColor: "lightblue", width: 350, padding: 3}}>
+            <Paper sx={{ backgroundColor: "lightblue", width: 350, padding: 3 }}>
                 <Typography variant="h2" component="h2">
                     Account
                 </Typography>
@@ -62,9 +181,10 @@ const AccountPage = () => {
                 <Typography variant="h4" component="h4">
                     {email}
                 </Typography>
-                <Typography variant="p" component="p">
+                <Typography variant="body1" component="p">
                     {description}
                 </Typography>
+                {updateDia()}
             </Paper>
         </Box>
     );
